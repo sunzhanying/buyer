a3551841b300553c8bd6cf5e2aaf70922425e610
sunzy <32124308+sunzhanying@users.noreply.github.com>
2020-06-16 22:09:14 +0800

    配置

--- utils/config.js
+++ utils/config.js
@@ -5,7 +5,9 @@
 // const NODOMAIN = 'http://quanyi.lemondm.com'
 
 
-const DOMAIN = 'https://quanyi.lemondm.com/Equity'
-const NODOMAIN = 'https://quanyi.lemondm.com'
+//const DOMAIN = 'https://quanyi.lemondm.com/Equity'
+const DOMAIN = 'http://localhost/Equity'
+//const NODOMAIN = 'https://quanyi.lemondm.com'
+const NODOMAIN = 'http://localhost'
 module.exports.DOMAIN = DOMAIN
 exports.NODOMAIN = NODOMAIN
