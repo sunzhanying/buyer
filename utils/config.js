ffa399a1cffb0e11c6aace606a5e4f8a020841b3
sunzy <32124308+sunzhanying@users.noreply.github.com>
2020-06-24 15:35:04 +0800

    修改买家配置

--- utils/config.js
+++ utils/config.js
@@ -1,15 +1,9 @@
 
-// const DOMAIN = 'http://192.168.31.245:8986/Equity'
-
-// const DOMAIN = 'http://quanyi.lemondm.com/Equity'
-// const NODOMAIN = 'http://quanyi.lemondm.com'
-
-
-//const DOMAIN = 'https://quanyi.lemondm.com/Equity'
 const DOMAIN = 'https://localhost/Equity'
-//const DOMAIN = 'https://quan.whaleupgo.com/Equity'
-//const NODOMAIN = 'https://quanyi.lemondm.com'
 const NODOMAIN = 'https://localhost'
+
+//const DOMAIN = 'https://quan.whaleupgo.com/Equity'
 //const NODOMAIN = 'https://quan.whaleupgo.com'
+
 module.exports.DOMAIN = DOMAIN
 exports.NODOMAIN = NODOMAIN
